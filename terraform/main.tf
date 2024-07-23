d2702d9eda351ce17c56f0445cc7238981b9d45f
dollervar <dollervar@github.com>
2024-07-24 00:21:17 +0200

    Add terraform code to build a ubuntu vm and configure basic settings

---
+++ terraform/main.tf
@@ -0,0 +1,82 @@
+terraform {
+ required_version = ">= 0.13"
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.7.6"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
+###############################################################################
+
+resource "libvirt_pool" "ubuntu-terraform" {
+  name = "ubuntu-terraform"
+  type = "dir"
+  path = "/var/lib/libvirt/terraform-provider-libvirt-pool-ubuntu-terraform"
+}
+
+resource "libvirt_volume" "ubuntu-vm-base" {
+  name   = "ubuntu-vm-base"
+  pool   = libvirt_pool.ubuntu-terraform.name
+  source = "https://cloud-images.ubuntu.com/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.img"
+  #source = "/var/lib/libvirt/terraform/base_images/ubuntu-24.04-server-cloudimg-amd64.img"
+  format = "qcow2"
+}
+
+resource "libvirt_cloudinit_disk" "commoninit" {
+  name           = "commoninit.iso"
+  user_data	 = templatefile("${path.module}/templates/user_data.yaml", {
+			hostname 	= var.hostname
+			username 	= var.username
+			password_hash	= var.user_password_hash
+                        ssh_pub_key 	= var.user_ssh_pub_key
+			})
+  network_config= templatefile("${path.module}/templates/network_config.yaml", {
+			ext_ip_addr	= var.ext_ip_addr
+			ext_ip_netmask	= var.ext_ip_netmask
+			ext_ip_gateway	= var.ext_ip_gateway
+			int_ip_addr	= var.int_ip_addr
+			int_ip_netmask	= var.int_ip_netmask
+			})
+  pool           = libvirt_pool.ubuntu-terraform.name
+}
+###############################################################################
+# Create the machine
+resource "libvirt_domain" "ubuntuvm" {
+  name   = "ubuntuvm"
+  memory = "512"
+  vcpu   = 1
+
+  cloudinit = libvirt_cloudinit_disk.commoninit.id
+
+  network_interface {
+    network_name = "default"
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+ 
+  disk {
+    volume_id = libvirt_volume.ubuntu-vm-base.id
+  }
+
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,49 @@
+variable "hostname" {
+  type	  = string
+  default = "ubuntuvm01"
+}
+
+variable "username" {
+  type	  = string
+  default = "toor"
+}
+
+variable "user_password_hash" {
+  type	  = string
+  default = "$6$x7ru7Q46frz7H2S0$1vrpgX0RlO.yWgrEGoqVt01dRPxtkJiPpLSdYe7pZW642WocXAYOnoxjCvMa8SKggo0IvAakKF1.3Iq3oI3CA1"
+}
+
+variable "user_ssh_pub_key" {
+  type	  = string
+  default = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNKEtzxRf0JPPaCmbF8TnFNCk3ohhbnBuheDagLF36/CMMcMm8XmnalYJqWLHOeGWY8yKysyFnWxJmoe+wOlMEI= smak@splashbook"
+}
+
+variable "ext_ip_addr" {
+  type	  = any
+  default = "192.168.122.146"
+}
+
+variable "ext_ip_netmask" {
+  type	  = any
+  default = "24"
+}
+
+variable "ext_ip_gateway" {
+  type	  = any
+  default = "192.168.122.1"
+}
+
+variable "int_ip_addr" {
+  type	  = string
+  default = "10.200.16.98"
+}
+
+variable "int_ip_netmask" {
+  type	  = string
+  default = "29"
+}
+
+variable "int_ip_gateway" {
+  type	  = string
+  default = "10.200.16.97"
+}
